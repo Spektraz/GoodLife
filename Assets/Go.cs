f3b4857f6b9f15586d6d48d76de9a53bea7eaa7d
Vlad Fotesko <foteskin18@gmail.com>
2019-12-10 23:59:44 +0200

    first commit

---
+++ Assets/Go.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Go : MonoBehaviour
+{
+    public GameObject Hero;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.W))
+	    {
+	        Hero.transform.Translate(Vector3.forward /2 );
+	    }
+
+	    if (Input.GetKey(KeyCode.S))
+	    {
+	        Hero.transform.Translate(Vector3.back /2);
+        }
+
+	    if (Input.GetKey(KeyCode.A))
+	    {
+            Hero.transform.Rotate(0,-1,0);
+	    }
+
+	    if (Input.GetKey(KeyCode.D))
+	    {
+	        Hero.transform.Rotate(0, 1, 0);
+        }
+	}
+}
---
+++ Assets/LightOner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightOner : MonoBehaviour
+{
+    private Light light;
+	// Use this for initialization
+	void Start ()
+	{
+	    light = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.Space))
+	    {
+	        light.enabled = false;
+            
+	    }
+
+	    if (Input.GetKey(KeyCode.F))
+	    {
+	        light.enabled = true;
+	    }
+	}
+}
